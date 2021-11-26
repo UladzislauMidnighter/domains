9b2c9b7da4eeaf5bc5427402981237da745038b8
MiDNiGHTERby <uladzislau.a@gmail.com>
2021-11-26 12:18:10 +0300

    to INT

--- project/main.js
+++ project/main.js
@@ -1,2 +1,15 @@
-var a = ["a", "b", "c"];
-alert (a[2]);
+function convertToInteger(x, base) {
+  const a = parseInt(x, base);
+  const b = "Это конвертировать нельзя";
+  if (isNaN(a)) { return b; }
+  return a;
+}
+
+console.log(convertToInteger('0xF', 16));
+// Вернет 15
+
+console.log(convertToInteger('321', 10));
+// Вернет 321
+
+console.log(convertToInteger('Пишу какой текст', 10));
+// Вернет "Это конвертировать нельзя"
